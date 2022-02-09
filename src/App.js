6aa71accc41e2d5c99fa1c3eb01db821f3d3ba98
navnit0707 <knavnit81@gmail.com>
2022-02-10 00:53:54 +0530

    it takes alot of time just because i forgot a simple things that component name should start with capital

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React, { Component } from 'react';
+import LogoNameHeader from './component/mainheader/logoName.js';
+
+class App extends Component{
+  render(){
+    return <LogoNameHeader />;
+  }
+}
+export default App;
---
+++ src/component/mainheader/logoName.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class LogoNameHeader extends Component{
+    render(){
+      return (
+        <div>
+            hi
+        </div>
+      );
+    }
+  }
+export default LogoNameHeader;
